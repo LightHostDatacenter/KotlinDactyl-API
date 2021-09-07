a71f02f4dbbd62370c183e2f029ec0b8f95c0971
Thales Belinazzi <thales@lighthost.com.br>
2021-09-07 10:36:34 -0300

    Added Client Network manager support

--- lib/src/main/kotlin/kotlinDactyl/client/ClientServer.kt
+++ lib/src/main/kotlin/kotlinDactyl/client/ClientServer.kt
@@ -3,6 +3,7 @@ package kotlinDactyl.client
 import kotlinDactyl.client.databases.ClientDatabaseManager
 import kotlinDactyl.client.details.ClientServerDetails
 import kotlinDactyl.client.files.ClientFileManager
+import kotlinDactyl.client.network.ClientNetworkManager
 import kotlinDactyl.requests.BaseRequest
 import org.json.JSONObject
 
@@ -13,5 +14,6 @@ class ClientServer (jsonResponse:String, baseRequest:BaseRequest) {
     val details: ClientServerDetails = server
     val fileManager = ClientFileManager(server, baseRequest)
     val databaseManager = ClientDatabaseManager(server, baseRequest)
+    val networkManager = ClientNetworkManager(server, baseRequest)
 
 }
